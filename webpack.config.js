e096c27b7bc3243378342574882e1ebf4514ba1e
Erik Berkun-Drevnig <erik.b22@gmail.com>
2016-07-09 23:02:56 -0400

    Add resolve alias to reduce file size

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
-var path = require('path')
-var webpack = require('webpack')
+    var path = require('path')
+    var webpack = require('webpack')
 
-module.exports = {
-  entry: './src/main.js',
+    module.exports = {
+      entry: './src/main.js',
 
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'bundle.min.js'
-  },
+      output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'bundle.min.js'
+      },
 
-  resolveLoader: {
-    root: path.join(__dirname, 'node_modules')
-  },
+      resolveLoader: {
+        root: path.join(__dirname, 'node_modules')
+      },
 
-  module: {
-    loaders: [
-      {
-        test: /\.handlebars$/,
-        loader: 'handlebars-loader'
-      }
-    ]
-  },
+      resolve: {
+        alias: {
+          moment: path.join(__dirname, 'node_modules/moment/min/moment.min.js')
+        }
+      },
 
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ]
-}
+      module: {
+        loaders: [
+          {
+            test: /\.handlebars$/,
+            loader: 'handlebars-loader'
+          }
+        ]
+      },
+
+      plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            warnings: false
+          }
+        }),
+
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /de|fr|hu/)
+      ]
+    }
