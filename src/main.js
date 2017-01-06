082e43f0a75a16b2eaeb483d5bbbb32471a14b94
Erik Berkun-Drevnig <erik.b22@gmail.com>
2017-01-06 03:17:04 -0500

    Removed jQuery dependency

--- src/main.js
+++ src/main.js
@@ -2,18 +2,19 @@ import moment from 'moment'
 import template from './template.handlebars'
 import handlebars from 'handlebars/runtime'
 import twitter from 'twitter-text'
+import axios from 'axios'
 
-(function () {
-  handlebars.registerHelper('prettyDate', (timestamp) => {
-    return moment(new Date(timestamp)).fromNow()
-  })
+handlebars.registerHelper('prettyDate', timestamp => {
+  return moment(new Date(timestamp)).fromNow()
+})
 
-  handlebars.registerHelper('autoLink', (tweet) => {
-    return twitter.autoLink(tweet)
-  })
+handlebars.registerHelper('autoLink', tweet => {
+  return twitter.autoLink(tweet)
+})
 
-  $.get('tweets.json', (data) => {
-    let html = template(data)
-    $('#twitter-timeline').html(html)
-  })
-})()
+axios.get('tweets.json').then(response => {
+  let container = document.getElementById('twitter-timeline')
+  container.innerHTML = template(response)
+}).catch(error => {
+  console.error(error)
+})
