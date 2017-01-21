3cf3f031c08389e3473d074e350def22309fdd39
Erik Berkun-Drevnig <erik.b22@gmail.com>
2017-01-21 00:57:16 -0500

    Upgraded webpack version, minimized syntax

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import moment from 'moment'
-import template from './template.handlebars'
+import template from './template.js'
 import handlebars from 'handlebars/runtime'
-import twitter from 'twitter-text'
-import axios from 'axios'
+import { autoLink } from 'twitter-text'
+import { get } from 'axios'
 
 handlebars.registerHelper('prettyDate', timestamp => {
   return moment(new Date(timestamp)).fromNow()
 })
 
 handlebars.registerHelper('autoLink', tweet => {
-  return twitter.autoLink(tweet)
+  return autoLink(tweet)
 })
 
-axios.get('tweets.json').then(response => {
+get('tweets.json').then(response => {
   let container = document.getElementById('twitter-timeline')
   container.innerHTML = template(response.data)
 }).catch(error => {
